faedd998f4b460c060ec43411fddc8fbaca897d6
Atheria <diaviekone13@gmail.com>
2025-09-02 12:37:03 +0700

    GPL

--- include/stdarg.h
+++ include/stdarg.h
@@ -1,12 +1,10 @@
-#ifndef STDARG_H
-#define STDARG_H
-
-// Custom stdarg implementation for freestanding environments (GCC/Clang)
+#ifndef VF_STDARG_H
+#define VF_STDARG_H
 
 typedef __builtin_va_list va_list;
+#define va_start(v, l)	__builtin_va_start(v, l)
+#define va_end(v)	__builtin_va_end(v)
+#define va_arg(v, T)	__builtin_va_arg(v, T)
+#define va_copy(d, s)	__builtin_va_copy(d, s)
 
-#define va_start(v,l)   __builtin_va_start(v,l)
-#define va_arg(v,t)     __builtin_va_arg(v,t)
-#define va_end(v)       __builtin_va_end(v)
-
-#endif // STDARG_H
+#endif // VF_STDARG_H
