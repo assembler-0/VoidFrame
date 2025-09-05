ea3bbb8f6bdf3a61ae173b0ac0be458889992c26
Atheria <diaviekone13@gmail.com>
2025-09-05 19:10:36 +0700

    VirtIO drivers

--- drivers/virtio/VirtioBlk.h
+++ drivers/virtio/VirtioBlk.h
@@ -1,12 +1,10 @@
 #ifndef VOIDFRAME_VIRTIOBLK_H
 #define VOIDFRAME_VIRTIOBLK_H
 
-#include "Virtio.h"
 #include "PCI/PCI.h"
-
-// VirtIO Block device specific definitions and function prototypes.
 
 void InitializeVirtioBlk(PciDevice device);
 int VirtioBlkRead(uint64_t sector, void* buffer);
+int VirtioBlkWrite(uint64_t sector, void* buffer);
 
 #endif //VOIDFRAME_VIRTIOBLK_H
